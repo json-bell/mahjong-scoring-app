67fd69b58e466362509492fd86e4f9eefeac0e14
Jason Bell <jasonbell143@gmail.com>
2025-10-29 02:08:55 +0000

    Dev (#6)
    * fixes colouring to match other tiles
    
    * MAH-33 Feat: Creates Colours and Colour Scheme
    
    * MAH-36 Feat: Creates Header and Layout, moving the API status checker into it
    
    * standardises some hand/meld inputs
    
    * lints and checks types
    
    * includes cx merging classNames
    
    * Hotfix: Safari grid displays for MeldPreview
    
    * makes modal scrollable
    
    * Improves MeldInput - input flow through tabs and restyles with new colour scheme
    
    * puts pair into the MeldInput
    
    * Adds ability for several modal buttons, splits MeldInput exits into Save and Close
    
    * adds label/placeholder to each tab within MeldInput
    
    * restyles Header for small screens
    
    * Hotfix: uncomments ready status
    
    * Hotfix: uncomments background
    
    * MAH-43 Feat: Rules page (#5)
    
    * Hotfix: uncomments comments
    
    * updates openapi-ts
    
    * implements Rules page list
    
    * Basic Menu and 404 page
    
    * Improves linking between & away from page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,9 @@
-import { HandScorer } from "../components/HandScorer/HandScorer";
+import ScorePage from "./ScorePage";
 
 const HomePage: React.FC = () => {
   return (
     <>
-      <div>
-        Mahjong Scoring - App Work in progress. You can check out the GitHub
-        project{" "}
-        <a href="https://github.com/json-bell/mahjong-scoring-app">here</a>
-      </div>
-
-      <HandScorer />
+      <ScorePage />
     </>
   );
 };
