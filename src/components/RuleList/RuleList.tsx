d862ee57b96f10f3f02f196a7610fef99535eede
Jason Bell <jasonbell143@gmail.com>
2025-10-29 01:25:43 +0000

    implements Rules page list

---
+++ src/components/RuleList/RuleList.tsx
@@ -0,0 +1,57 @@
+import { useMemo } from "react";
+import useScoringRules from "../../hooks/api/useScoringRules";
+import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
+import styles from "./RuleList.module.scss";
+
+const RuleList = () => {
+  const { rules, error, loading } = useScoringRules();
+
+  const sortedRules = useMemo(
+    () =>
+      [...rules].sort(
+        ({ score_value: scoreA }, { score_value: scoreB }) => scoreA - scoreB
+      ),
+    [rules]
+  );
+
+  return (
+    <div>
+      <h2>Here are the rules used:</h2>
+      {loading ? (
+        <LoadingSpinner />
+      ) : error ? (
+        "We encountered an error. Please try again later"
+      ) : (
+        <ul className={styles.ruleList}>
+          {sortedRules.map(({ description, score_value, slug, supersedes }) => (
+            <li key={slug} id={slug} className={styles.rule}>
+              <h3 className={styles.ruleTitle}>
+                {slug} - value: {score_value}
+              </h3>
+              {description}
+              {supersedes?.length === 0 ? null : (
+                <>
+                  <br />
+                  <br />
+                  Supersedes:
+                  <ul className={styles.supersedesList}>
+                    {supersedes?.map((slug) => {
+                      return (
+                        <li key={slug}>
+                          <a href={`#${slug}`}>{slug}</a>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </>
+              )}
+              {/* <MeldPreview tiles={Array(14).fill({ miscTileSlug: "any" })} /> */}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default RuleList;
